d70932e85daae83ab5ad698152fadba3cd58b43c
Timo Savola <timo.savola@iki.fi>
2014-12-20 22:04:02 +0200

    test changing

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import gc
 import sys
 
 import tap
@@ -10,24 +11,27 @@ def main():
 		f = test2
 
 	f()
+	gc.collect()
 
 def test1():
-	def func(obj, xprint, out):
-		xprint(obj, file=out)
-		xprint(sys.prefix, file=out)
-		xprint(sys.maxsize, file=out)
-		builtinfunc = obj[-1]
-		xprint(builtinfunc())
+	def func(obj):
+		print("obj:", obj)
+		print("sys.prefix:", sys.prefix)
+		print("sys.maxsize:", sys.maxsize)
+		getrefcount = obj[-1]
+		print("refcount:", getrefcount(None))
 
-	print(tap.core.Opaque(), file=sys.stderr)
+	print("opaque:", tap.core.Opaque())
 
 	peer = tap.Peer()
+	print("peer:", peer)
+
 	buf = bytearray()
 	t = (5435452652,)
-	obj = (func, (1324, 5435432, t, None), (54325235, 9, t, 765376542, None, 9), sys.exc_info)
+	obj = (func, (1324, 5435432, t, None), (54325235, 9, t, 765376542, None, 9), sys.getrefcount)
 	tap.marshal(peer, buf, obj)
 
-	print("{} bytes".format(len(buf)), file=sys.stderr)
+	print("size:", len(buf))
 
 	with open(sys.argv[2], "wb") as f:
 		f.write(buf)
@@ -40,7 +44,7 @@ def test2():
 	obj = tap.unmarshal(peer, data)
 
 	func = obj[0]
-	func(obj, print, sys.stderr)
+	func(obj)
 
 if __name__ == "__main__":
 	main()
